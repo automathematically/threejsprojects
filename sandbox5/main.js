131a3327b71455963af13d6e5f7f2c89881a5446
mathieuvanleemputten <mathieuvanleemputten@hotmail.com>
2020-04-13 18:45:52 +0200

    c

--- sandbox5/main.js
+++ sandbox5/main.js
@@ -3,10 +3,10 @@ function init() {
 	var gui = new dat.GUI();
 
 	// initialize objects
-	var sphereMaterial = getMaterial('basic', 'rgb(255, 0, 0)');
+	var sphereMaterial = getMaterial('standard', 'rgb(255, 255, 255)');
 	var sphere = getSphere(sphereMaterial, 1, 24);
 
-	var planeMaterial = getMaterial('basic', 'rgb(0, 0, 255)');
+	var planeMaterial = getMaterial('standard', 'rgb(255, 255, 255)');
 	var plane = getPlane(planeMaterial, 30);
 
 	var lightLeft = getSpotLight(1, 'rgb(255, 220, 180)');
@@ -25,6 +25,14 @@ function init() {
 	lightRight.position.z = -4;
 
 	// manipulate materials
+	var loader = new THREE.TextureLoader();
+	planeMaterial.map = loader.load('/assets/textures/concrete.jpg');
+
+	var texture = planeMaterial.map;
+	texture.wrapS = THREE.RepeatMapping;
+	texture.wrapT = THREE.RepeatMapping;
+	texture.repeat.set(1.5,1.5);
+
 
 	// dat.gui
 	var folder1 = gui.addFolder('light_1');
@@ -38,6 +46,13 @@ function init() {
 	folder2.add(lightRight.position, 'x', -5, 15);
 	folder2.add(lightRight.position, 'y', -5, 15);
 	folder2.add(lightRight.position, 'z', -5, 15);
+
+	var folder3 = gui.addFolder('materials');
+	folder3.add(sphereMaterial, 'roughness', 0, 1);
+	folder3.add(planeMaterial, 'roughness', 0, 1);
+	folder3.add(sphereMaterial, 'metalness', 0, 1);
+	folder3.add(planeMaterial, 'metalness', 0, 1);
+	folder3.open();
 
 	// add objects to the scene
 	scene.add(sphere);
@@ -112,8 +127,8 @@ function getSpotLight(intensity, color) {
 	light.penumbra = 0.5;
 
 	//Set up shadow properties for the light
-	light.shadow.mapSize.width = 1024;  // default: 512
-	light.shadow.mapSize.height = 1024; // default: 512
+	light.shadow.mapSize.width = 2048;  // default: 512
+	light.shadow.mapSize.height = 2048; // default: 512
 	light.shadow.bias = 0.001;
 
 	return light;
